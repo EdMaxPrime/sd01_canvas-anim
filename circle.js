8fa41ef50420226a087bb08bd5397c2e53760378
Max Z <max.zlotskiy@gmail.com>
2018-02-07 11:30:27 -0500

    wrote functions for animation

--- circle.js
+++ circle.js
@@ -1,17 +1,29 @@
 var canvas = document.getElementById("anim");
 var ctx = canvas.getContext("2d");
-var x = 0;
+var radius = 0;
+var rate = 0;
 var requestID;
+
+var update = function() {
+    radius += rate;
+    if(radius >= canvas.width || radius <= 0) {
+	rate *= -1;
+    }
+}
 
 var drawCircle = function() {
     ctx.clearRect(0, 0, 500, 500);
     ctx.beginPath();
-    ctx.arc(x, 250, 50, 0, 2 * Math.PI);
+    ctx.arc(250, 250, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "green";
     ctx.stroke();
     ctx.fill();
-    x++;
+}
+
+var animationLoop = function() {
+    drawCircle();
+    update();
     requestID = window.requestAnimationFrame(drawCircle);
 }
 
-window.requestAnimationFrame(drawCircle);
+window.requestAnimationFrame(animationLoop);
