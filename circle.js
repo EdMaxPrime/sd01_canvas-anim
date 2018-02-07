2d600669f7dbe0582ab1ce2e67f79b56f5fae5be
Max Z <max.zlotskiy@gmail.com>
2018-02-07 18:09:30 -0500

    attached event listeners to buttons and wrote html file

--- circle.js
+++ circle.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("anim");
+var canvas = document.getElementById("tabula_rasa");
 var ctx = canvas.getContext("2d");
 var radius = 0;
 var rate = 0;
@@ -6,32 +6,37 @@ var requestID;
 
 var update = function() {
     radius += rate;
-    if(radius >= canvas.width || radius <= 0) {
-	rate *= -1;
+    if(radius >= canvas.width/2 || radius <= 0) {
+	    rate *= -1;
     }
-}
+};
 
 var drawCircle = function() {
-    ctx.clearRect(0, 0, 500, 500);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(250, 250, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "green";
+    ctx.arc(canvas.width/2, canvas.height/2, radius, 0, 2 * Math.PI);
+    if(rate > 0) ctx.fillStyle = "green";
+    else         ctx.fillStyle = "yellow";
     ctx.stroke();
     ctx.fill();
-}
+};
 
 var animationLoop = function() {
     drawCircle();
     update();
-    requestID = window.requestAnimationFrame(drawCircle);
-}
+    requestID = window.requestAnimationFrame(animationLoop);
+};
 
 var startListener = function() {
-    requestID = window.requestAnimationFrame(animationLoop);
+    radius = 0;
     rate = 1;
-}
+    requestID = window.requestAnimationFrame(animationLoop);
+};
 
 var stopListener = function() {
     window.cancelAnimationFrame(requestID);
     rate = 0;
-}
+};
+
+document.getElementById("start").addEventListener("click", startListener);
+document.getElementById("stop").addEventListener("click", stopListener);
