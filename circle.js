a247362d75ceb9b0d444116d9cba9e3db8c956aa
Max Z <max.zlotskiy@gmail.com>
2018-02-08 14:48:18 -0500

    added dvd bouncing logo animation

--- circle.js
+++ circle.js
@@ -2,35 +2,50 @@ var canvas = document.getElementById("tabula_rasa");
 var ctx = canvas.getContext("2d");
 var radius = 0;
 var rate = 0;
+var x = 250, y = 250, speedX = 2, speedY = 1;
 var requestID;
-
-var update = function() {
-    radius += rate;
-    if(radius >= canvas.width/2 || radius <= 0) {
-	    rate *= -1;
-    }
-};
 
 var drawCircle = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(canvas.width/2, canvas.height/2, radius, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
     if(rate > 0) ctx.fillStyle = "green";
     else         ctx.fillStyle = "yellow";
     ctx.stroke();
     ctx.fill();
 };
 
-var animationLoop = function() {
+var growShrinkLoop = function() {
     drawCircle();
-    update();
-    requestID = window.requestAnimationFrame(animationLoop);
+    radius += rate;
+    if(radius >= canvas.width/2 || radius <= 0) {
+        rate *= -1;
+    }
+    requestID = window.requestAnimationFrame(growShrinkLoop);
 };
 
-var startListener = function() {
-    radius = 0;
-    rate = 1;
-    requestID = window.requestAnimationFrame(animationLoop);
+var dvdBounceLoop = function() {
+    drawCircle();
+    x += speedX;
+    y += speedY;
+    if(x - radius <= 0 || x + radius >= canvas.width) {speedX *= -1;}
+    if(y - radius <= 0 || y + radius >= canvas.height) {speedY *= -1;}
+    requestID = window.requestAnimationFrame(dvdBounceLoop);
+}
+
+var startListener = function(evt) {
+    x = canvas.width/2;
+    y = canvas.height/2;
+    if(evt.srcElement.id == "start1") {
+	radius = 0;
+        rate = 1;
+        requestID = window.requestAnimationFrame(growShrinkLoop);
+    } else {
+	radius = 20;
+	speedX = 2;
+	speedY = 1;
+	requestID = window.requestAnimationFrame(dvdBounceLoop);
+    }
 };
 
 var stopListener = function() {
@@ -38,5 +53,6 @@ var stopListener = function() {
     rate = 0;
 };
 
-document.getElementById("start").addEventListener("click", startListener);
+document.getElementById("start1").addEventListener("click", startListener);
+document.getElementById("start2").addEventListener("click", startListener);
 document.getElementById("stop").addEventListener("click", stopListener);
